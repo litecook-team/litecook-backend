55a1c16998a6ecdc4c636e4c8da196a70cc97597
Honcharuk Bohdan <teofipol_sv@ukr.net>
2026-04-04 16:47:05 +0300

    increased refresh token lifetime to 30 days for user convenience

--- core/settings.py
+++ core/settings.py
@@ -159,7 +159,7 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 

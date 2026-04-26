822f43862397cf8914d38744c8b6435b6691eff4
Honcharuk Bohdan <teofipol_sv@ukr.net>
2026-04-26 14:36:32 +0300

    Updated urls.py -> core, warning.html

--- core/urls.py
+++ core/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import RedirectView
@@ -46,3 +46,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# === ГОЛОВНА ПАСТКА (ЗАВЖДИ В САМОМУ КІНЦІ) ===
+# Перехоплює будь-які URL, які не підійшли під жоден з маршрутів вище.
+urlpatterns.append(re_path(r'^.*$', TemplateView.as_view(template_name='warning.html')))
